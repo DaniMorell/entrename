1be3065ded519751522370acfc1cd8318d5ac0b5
DaniMorell <jdmorell@hotmail.com>
2015-06-12 13:04:20 +0200

    primer entrene

---
+++ controllers/misentrenes_controller.js
@@ -0,0 +1,3 @@
+exports.entrene = function(req, res){
+	res.render('entrenes/entrene', {entrene: 'Entrene Nº 1'});
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+var misentrenescontroller= require('../controllers/misentrenes');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Entrena Me' });
 });
+
+router.get('/entrenes/entrene', misentrenescontroller.entrene);
 
 module.exports = router;
