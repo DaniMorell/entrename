b03f3959b818499063822caf5bbf17d7a92b17f7
DaniMorell <jdmorell@hotmail.com>
2015-06-15 12:38:10 +0200

    Cambios 1.0.1

--- controllers/misentrenes_controller.js
+++ controllers/misentrenes_controller.js
@@ -1,7 +1,92 @@
 var models = require('../models/models.js');
 
-exports.entrene = function(req, res){
-	models.Entrene.findAll().success(function(misentrenes) {
-		res.render('entrenes/entrene', {entrenamiento: misentrenes[0].entrenamiento});
+// Autoload :id
+exports.load = function(req, res, next, entreneId) {
+  models.Entrene.find({
+            where: {
+                id: Number(entreneId)
+            },
+            include: [{
+                model: models.Comment
+            }]
+        }).then(function(entrene) {
+      if (entrene) {
+        req.entrene = entrene;
+        next();
+      } else{next(new Error('No existe entreneId=' + entreneId))}
+    }
+  ).catch(function(error){next(error)});
+};
+
+exports.index = function(req, res){
+	models.Entrene.findAll().success(function(entrene) {
+		res.render('entrenes/index.ejs', {entrene: entrene});
 	})	
+};
+
+// GET /quizes/:id
+exports.show = function(req, res) {
+  res.render('entrenes/show', { entrene: req.entrene, errors: []});
+};            // req.quiz: instancia de quiz cargada con autoload
+
+// GET /quizes/new
+exports.new = function(req, res) {
+  var entrene = models.Entrene.build( // crea objeto quiz 
+    {entrenamiento: "Entrenamiento"}
+  );
+
+  res.render('entrenes/new', {entrene: entrene, errors: []});
+};
+
+// POST /quizes/create
+exports.create = function(req, res) {
+  var entrene = models.Entrene.build( req.body.entrene );
+
+  entrene
+  .validate()
+  .then(
+    function(err){
+      if (err) {
+        res.render('entrenes/new', {entrene: entrene, errors: err.errors});
+      } else {
+        entrene // save: guarda en DB campos pregunta y respuesta de quiz
+        .save({fields: ["entrenamiento"]})
+        .then( function(){ res.redirect('/entrenes/entrene')}) 
+      }      // res.redirect: Redirección HTTP a lista de preguntas
+    }
+  );
+};
+
+// GET /entrenes/:id/edit
+exports.edit = function(req, res) {
+  var entrene = req.entrene;  // req.entrene: autoload de instancia de entrene
+
+  res.render('entrenes/edit', {entrene: entrene, errors: []});
+};
+
+// PUT /quizes/:id
+exports.update = function(req, res) {
+
+  req.entrene.entrenamiento  = req.body.entrene.entrenamiento;
+
+  req.entrene
+  .validate()
+  .then(
+    function(err){
+      if (err) {
+        res.render('entrenes/edit', {entrene: req.entrene, errors: err.errors});
+      } else {
+        req.entrene     // save: guarda campos pregunta y respuesta en DB
+        .save( {fields: ["entrenamiento"]})
+        .then( function(){ res.redirect('/entrenes/entrene');});
+      }     // Redirección HTTP a lista de preguntas (URL relativo)
+    }
+  );
+};
+
+// DELETE /quizes/:id
+exports.destroy = function(req, res) {
+  req.entrene.destroy().then( function() {
+    res.redirect('/entrenes');
+  }).catch(function(error){next(error)});
 };
--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,18 @@ var misentrenescontroller= require('../controllers/misentrenes_controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Entrena Me' });
+  res.render('index', { title: 'Entrena Me', errors: []});
 });
 
-router.get('/entrenes/entrene', misentrenescontroller.entrene);
+router.param('entreneId', misentrenescontroller.load);  // autoload :misentrenesId
+
+router.get('/entrenes/entrene', misentrenescontroller.index);
+router.get('/entrenes/:entreneId(\\d+)',        misentrenescontroller.show);
+
+router.get('/entrenes/new', 				   misentrenescontroller.new);
+router.post('/entrenes/create',              misentrenescontroller.create);
+router.get('/entrenes/:entreneId(\\d+)/edit',    misentrenescontroller.edit);
+router.put('/entrenes/:entreneId(\\d+)',       misentrenescontroller.update);
+router.delete('/entrenes/:entreneId(\\d+)',     misentrenescontroller.destroy);
 
 module.exports = router;
